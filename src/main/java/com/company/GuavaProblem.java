fe8fd0eeb7e9c65defa050541a7fcbfce607b7c2
Andrew Matheny <andrew.matheny@listhub.com>
2013-08-06 10:29:39 -0400

    Initial commit

---
+++ src/main/java/com/company/GuavaProblem.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.AbstractIterator;
+
+public class GuavaProblem {
+    public static void main(String[] args) {
+    }
+}
